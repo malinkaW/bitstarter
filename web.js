35c195ce994c8eaff82bb3583d657ce392de234e
Malinka Walaliyadde <rmw89@cornell.edu>
2013-11-20 09:48:28 +0000

    Final update to web.js

--- web.js
+++ web.js
@@ -3,15 +3,6 @@ var app = express();
 var fs = require('fs');
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send("Hello World");
-});
-
-app.get('/', function(request1, response1) {
-  var read = fs.readFileSync('index.html','utf8');
-  response1.send(read);
-});
-
 app.get('/', function(request2, response2) {
   var read = fs.readFileSync('index.html');
   response2.send(read.toString());
