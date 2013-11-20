1265418ec12aac891327ccae5ef03d3bb1e61fe6
Malinka Walaliyadde <rmw89@cornell.edu>
2013-11-20 09:00:43 +0000

    Corrected web.js

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
-  fs.readFileSync(index.html);
-  response.send(buf.toString('utf-8'));
+  var read = fs.readFileSync('index.html');
+  response.send(read.toString());
 });
 
 var port = process.env.PORT || 5000;
