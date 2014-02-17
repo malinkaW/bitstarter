6f58e47aac5750515c571212dcc2fccf40764213
Malinka Walaliyadde <rmw89@cornell.edu>
2014-02-17 06:40:12 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
 var express = require('express');
-var app = express();
 var fs = require('fs');
-app.use(express.logger());
+var htmlfile = "index.html";
 
-app.get('/', function(request2, response2) {
-  var read = fs.readFileSync('index.html');
-  response2.send(read.toString());
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+ 
